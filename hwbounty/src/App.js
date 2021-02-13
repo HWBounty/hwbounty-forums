c9b5e8063200feccfb96892d365c7639ab478533
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2021-02-12 20:03:51 -0800

    init

--- hwbounty/src/App.js
+++ hwbounty/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themeFile from "./util/theme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Pages
+import home from './pages/home';
+
+const theme = createMuiTheme(themeFile);
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={null}>
+        <p>INIT!!!</p>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
---
+++ hwbounty/src/pages/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
+
+export class home extends Component {
+    render() {
+        return (
+            <Grid>
+                <p>Hello boys!</p>
+            </Grid>
+        );
+    }
+}
