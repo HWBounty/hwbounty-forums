22cea643ccabf2d438c93ed41c1b0dd1a8b505c6
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2021-02-13 01:13:57 -0800

    you dummy

--- hwbounty/src/pages/signup.js
+++ hwbounty/src/pages/signup.js
@@ -1,42 +1,162 @@
-// React
 import React, { Component } from "react";
-
-// Styling
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from "prop-types";
+import AppIcon from "../images/favicon.ico";
+import { Link } from "react-router-dom";
 
-// Redux
+//MUI Stuff
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+// Redux stuff
 import { connect } from "react-redux";
+import { signupUser } from "../redux/actions/userActions";
 
-export class Signup extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <br /> <br />
-                    <TextField id="outlined-basic" label="Username" /> <br /> 
-                    <TextField id="outlined-basic" label="Email" /> <br /> 
-                    <TextField
-                        id="outlined-basic"
-                        label="Password"
-                        type="password"
-                        onChange={this.handleChange}
-                    />
-                    <TextField
-                        id="outlined-basic"
-                        label="Confirm Password"
-                        type="password"
-                        onChange={this.handleConfirmChange}
-                    />
-                    <br /> 
-                    <Button variant="contained" color="primary"> Sign up </Button> <br /> <br />
+const styles = (theme) => ({
+  ...theme.spreadIt,
+});
 
-                    <Button variant="outlined">Sign up with Google</Button>
-                </header>
-            </div>
-        );
+export class signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      handle: "",
+      errors: {},
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.UI.errors) {
+      this.setState({ errors: nextProps.UI.errors });
     }
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    const newUserData = {
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword,
+      handle: this.state.handle,
+    };
+    this.props.signupUser(newUserData, this.props.history);
+  };
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  render() {
+    const {
+      classes,
+      UI: { loading },
+    } = this.props;
+    const { errors } = this.state;
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="hwbounty logo" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Signup
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              label="Confirm Password"
+              className={classes.textField}
+              helperText={errors.confirmPassword}
+              error={errors.confirmPassword ? true : false}
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="handle"
+              name="handle"
+              type="text"
+              label="Handle (Username)"
+              className={classes.textField}
+              helperText={errors.handle}
+              error={errors.handle ? true : false}
+              value={this.state.handle}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              disabled={loading}
+            >
+              Signup
+              {loading && (
+                <CircularProgress className={classes.progress} size={30} />
+              )}
+            </Button>
+            <br />
+            <small>
+              Already have an account ? login <Link to="/login">here</Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
+    );
+  }
 }
 
-export default Signup;
+signup.propTypes = {
+  classes: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+  UI: state.UI,
+});
+
+export default connect(mapStateToProps, { signupUser })(
+  withStyles(styles)(signup)
+);
