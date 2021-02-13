bebcb0bb10181dd180d9e1e89f024699f55a00ba
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2021-02-12 22:15:48 -0800

    stuff

--- hwbounty/src/components/layout/Navbar.js
+++ hwbounty/src/components/layout/Navbar.js
@@ -6,31 +6,51 @@ import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// Icons
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+// Images
+import HWBountyLogo from "../../images/HWBounty-Logo.png";
+
+const styles = (theme) => ({
+  ...theme.spreadIt,
+  root: {
+    flexGrow: 1,
+  },
+  logo: {
+    objectFit: "fill",
+    maxWidth: "100px",
+    maxHeight: "100px",
+  },
+});
 
 class Navbar extends Component {
   render() {
-    //const { authenticated } = this.props;
+    const { authenticated, classes } = this.props;
     return (
-      <AppBar>
-        <Toolbar>
-          <Button align="left" color="inherit" component={Link} to="/about">
-            About
-          </Button>
-          <div className="nav-container">
-            <Button color="inherit" component={Link} to="/login">
-              Login
-            </Button>
-            <Button color="inherit" component={Link} to="/">
-              Home
-            </Button>
-            <Button color="inherit" component={Link} to="/signup">
-              Signup
-            </Button>
-          </div>
-        </Toolbar>
-      </AppBar>
+      <div className={classes.root}>
+        <AppBar>
+          <Toolbar>
+            <div className="nav-container">
+              <img src={HWBountyLogo} className={classes.logo} />
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Signup
+              </Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
