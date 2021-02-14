c4d93b58aa21571dc06463055bbe8fcd9dbbe2ff
Ash Ranjan <ranjanashvin@gmail.com>
2021-02-14 02:13:59 -0800

    the comments section

--- hwbounty/src/pages/bountyview.js
+++ hwbounty/src/pages/bountyview.js
@@ -16,9 +16,11 @@ import CommentIcon from "@material-ui/icons/Comment";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import LikeButton from "../components/bounty/LikeButton";
 import BountyReward from "../components/bounty/BountyReward";
+import Grid from "@material-ui/core/Grid";
 import Chip from "@material-ui/core/Chip";
 import SendIcon from "@material-ui/icons/Send";
 import TextField from "@material-ui/core/TextField";
+import CheckIcon from '@material-ui/icons/Check';
 
 // Requests
 import axios from "axios";
@@ -34,6 +36,7 @@ const styles = (theme) => ({
     width: "50%",
     textAlign: "left",
     float: "left",
+    paddingBottom: 1,
   },
   textField: {
     margin: "15px auto 15px auto",
@@ -41,6 +44,13 @@ const styles = (theme) => ({
   },
   points: {
     right: "10%",
+  },
+  answerCheck: {
+    float: "right",
+  },
+  childComment: {
+    width: "95%",
+    float: "right",
   },
 });
 
@@ -72,12 +82,12 @@ export class bountyview extends Component {
       .then((res_obj) => {
         res_obj.data
           ? this.setState({
+              ...this.state,
               id: res_obj.data.bountyID,
               title: res_obj.data.title,
               text: res_obj.data.description,
               posterName: res_obj.data.author.publicID,
               posterIcon: res_obj.data.author.pfp,
-              comments: [],
               bounty: res_obj.data.points,
               claimed: false,
               labels: res_obj.data.tags.split(",").map((l, e) => {
@@ -85,13 +95,24 @@ export class bountyview extends Component {
               }),
             })
           : this.setState({ valid: false });
-        this.setState({
-          errors: {
-            handle: res_obj.data.data
-              ? "The handle is either taken or too short"
-              : null,
-          },
-        });
+        
+      });
+
+      axios
+      .get(
+        "/comments/" +
+          this.props.location.pathname.substring(
+            this.props.location.pathname.lastIndexOf("/") + 1
+          )
+      )
+      .then((res_obj) => {
+        res_obj.data
+          ? this.setState({ 
+            ...this.state,
+            comments: res_obj.data,
+          })
+          : this.setState({ valid: false });
+      
       });
   };
 
@@ -102,68 +123,82 @@ export class bountyview extends Component {
     return (
       <div className={classes.rootPadding}>
         {valid ? (
-          <Card className={classes.root}>
-            <CardHeader
-              avatar={
-                <Avatar
-                  aria-label={this.state.posterName}
-                  src={this.state.posterIcon}
+          <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12}>
+              <Card >
+                <CardHeader
+                  avatar={
+                    <Avatar
+                      aria-label={this.state.posterName}
+                      src={this.state.posterIcon}
+                    />
+                  }
+                  title={this.state.title}
+                  subheader={"Posted by: " + this.state.posterName}
                 />
-              }
-              title={this.state.title}
-              subheader={"Posted by: " + this.state.posterName}
-            />
-            <CardContent>
-              {this.state.labels.length > 0 ? (
-                <span>
-                  <span>Topics:</span> <br />
-                  {this.state.labels.map((l, i) => {
-                    return (
-                      <Chip
-                        label={l[0]}
-                        style={l[1]}
-                        component="a"
-                        href={"/?t=" + compactLabel(l[0])}
-                        clickable
-                      />
-                    );
-                  })}{" "}
-                  <br /> <br />
-                </span>
-              ) : null}
-
-              <Typography variant="body2" color="textSecondary" component="p">
-                {this.state.text.split("\n").map((l, i) => {
-                  return (
-                    <span id={i}>
-                      {l}
-                      <br />
+                <CardContent>
+                  {this.state.labels.length > 0 ? (
+                    <span>
+                      <span>Topics:</span> <br />
+                      {this.state.labels.map((l, i) => {
+                        return (
+                          <Chip
+                            label={l[0]}
+                            style={l[1]}
+                            component="a"
+                            href={"/?t=" + compactLabel(l[0])}
+                            clickable
+                          />
+                        );
+                      })}{" "}
+                      <br /> <br />
                     </span>
-                  );
-                })}
-              </Typography>
-              <BountyReward
-                pointReward={this.state.bounty}
-                claimed={this.state.claimed}
-              />
-            </CardContent>{" "}
-            <br /> <br />
-            <CardActions disableSpacing>
-              <TextField
-                id="title"
-                name="title"
-                type="title"
-                label="Title"
-                className={classes.textField}
-                variant="outlined"
-                size="small"
-                fullWidth
-              />
-              <IconButton>
-                <SendIcon color="primary" />
-              </IconButton>
-            </CardActions>
-          </Card>
+                  ) : null}
+
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {this.state.text.split("\n").map((l, i) => {
+                      return (
+                        <span id={i}>
+                          {l}
+                          <br />
+                        </span>
+                      );
+                    })}
+                  </Typography>
+                  <BountyReward
+                    pointReward={this.state.bounty}
+                    claimed={this.state.claimed}
+                  />
+                </CardContent>{" "}
+                <br /> <br />
+                <CardActions disableSpacing>
+                  <TextField
+                    id="title"
+                    name="title"
+                    type="title"
+                    label="Title"
+                    className={classes.textField}
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                  />
+                  <IconButton>
+                    <SendIcon color="primary" />
+                  </IconButton>
+                </CardActions>
+              </Card>
+            </Grid>
+            {this.state.comments.map((c, i) => 
+              <Grid item xs={12}>
+                <Card className={(c.parentCommentID != null ? classes.childComment : null)}>
+                  {c.comment} {(c.edited > 0 ? <small style={{color: "gray"}}>(edited)</small>: null)}
+                  {(c.parentCommentID != null ? null : <IconButton className={classes.answerCheck}>
+                    <CheckIcon color="disable" />
+                  </IconButton>)}
+                </Card>
+              </Grid>
+            )}
+          </Grid>
         ) : (
           <BountyNotFound />
         )}
